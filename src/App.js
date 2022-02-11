be9520b6045198372e938d9b60e55d8a6bb6e2f3
Alejandro Perez Banks <alejandroperezbanks@MacBook-Pro-de-Alejandro.local>
2022-02-11 17:29:50 +0000

    friday

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-
-
 import './App.css';
 import { useState } from 'react';
 
@@ -69,3 +67,26 @@ const App = () => {
   
 
 export default App
+
+
+
+// I did really tried to make the button done once clicked to change the text next to it change color, 
+// i tried doing new functions, called  edittext but I have no clue what to do..
+
+// const editText = () => {
+//   let storedItems = [...items]
+//   storedItems <--------- Im guessing in this line is where  the text goes to, but no idea what
+//                           i have to code to make the text imported here once clicked done to change color
+//   setItems(storedItems)
+
+// }
+
+// after I assume we would have to write this after return 
+
+
+// <button className = "button" onClick = {() => editText(index)}>Done</button>
+
+
+//  all this never worked... Im so lost with react and javascript in general 
+// future Alex will fix it probably.
+
